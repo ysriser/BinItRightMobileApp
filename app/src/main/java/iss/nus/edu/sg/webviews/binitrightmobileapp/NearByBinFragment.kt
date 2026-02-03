3619603b6197f6203b0f0f57641e6e64f7c9bf62
Muthu-raj22 <e1539335@u.nus.edu>
2026-02-04 01:39:45 +0800

    Refactored the code to use same end point for fetching both nearby bins and All recycling bins

--- app/src/main/java/iss/nus/edu/sg/webviews/binitrightmobileapp/NearByBinFragment.kt
+++ app/src/main/java/iss/nus/edu/sg/webviews/binitrightmobileapp/NearByBinFragment.kt
@@ -190,7 +190,7 @@ class NearByBinFragment : Fragment(R.layout.fragment_near_by_bin), OnMapReadyCal
                 val json = withContext(Dispatchers.IO) {
                     // Use emulator URL (10.0.2.2) for Android Emulator
                     // Use device URL (192.168.88.4) for physical device
-                    val urlString = "http://10.0.2.2:8080/api/bins/nearby?lat=$lat&lng=$lng&radius=3000"
+                    val urlString = "http://10.0.2.2:8080/api/bins/search?lat=$lat&lng=$lng&radius=3000"
 
                     Log.d(TAG, "Fetching from URL: $urlString")
 
