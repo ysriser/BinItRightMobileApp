c206cae4d347035234f94bb23ba397287e69ccab
Muthu-raj22 <e1539335@u.nus.edu>
2026-02-05 12:44:00 +0800

    user specific Recycle History

---
+++ app/src/main/java/iss/nus/edu/sg/webviews/binitrightmobileapp/model/RecycleHistoryModel.kt
@@ -0,0 +1,8 @@
+package iss.nus.edu.sg.webviews.binitrightmobileapp.model
+
+data class RecycleHistoryModel (
+    val categoryName: String,
+    val categoryIcon: String,
+    val date: String,
+    val quantity: Int
+)
